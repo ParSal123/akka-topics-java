3486f8c4c06f547d6418f8cfa74dfb0e5e871641
Parsa Salavati <p.salavati@mohaymen.ir>
2022-09-10 09:54:05 +0430

    Refactor ErrorKernelApp. Also add logback config

--- one-actor-is-no-actor/src/main/java/ErrorKernelApp.java
+++ one-actor-is-no-actor/src/main/java/ErrorKernelApp.java
@@ -1,18 +1,18 @@
-
-import java.io.IOException;
-import java.util.stream.Stream;
-
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.BehaviorBuilder;
 import akka.actor.typed.javadsl.Behaviors;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ErrorKernelApp {
 
     public static void main(String[] args) throws IOException {
         ActorSystem<Guardian.Command> guardian = ActorSystem.create(Guardian.create(), "error-kernel");
-        guardian.tell(new Guardian.Start(new String[]{"-one-", "--two--"}));
+        guardian.tell(new Guardian.Start(new ArrayList<>(Arrays.asList("-one-", "--two--"))));
 
         System.out.println("Press ENTER to terminate");
         System.in.read();
@@ -22,147 +22,91 @@ public class ErrorKernelApp {
 
 class Guardian {
 
-    interface Command {
+    public static Behavior<Command> create() {
+        return Behaviors.setup(context -> {
+            context.getLog().info("setting up. Creating manager");
+            ActorRef<Manager.Command> manager = context.spawn(Manager.create(), "manager-alpha");
+            return Behaviors.receiveMessage(msg -> {
+                if (msg instanceof Start start) {
+                    manager.tell(new Manager.Delegate(start.texts));
+                    return Behaviors.same();
+                } else return Behaviors.unhandled();
+            });
+            /* Can also be written like this:
+                    return BehaviorBuilder.<Command>create()
+                            .onMessage(
+                                    Guardian.Start.class,
+                                    start -> {
+                                        manager.tell(new Manager.Delegate(start.texts));
+                                        return Behaviors.same();
+                                    })
+                            .build();
+
+             */
+        });
     }
 
-//    static final class Start implements Command {
-//        String[] texts;
-//
-//        Start(String[] texts) {
-//            this.texts = texts;
-//        }
-//    }
-
-    record Start(String[] texts) implements Command {
+    public interface Command {
     }
 
-
-    static Behavior<Command> create() {
-        return Behaviors.setup(
-                context -> {
-                    context.getLog().info("setting up. Creating manager");
-                    ActorRef<Manager.Command> manager = context.spawn(Manager.create(), "manager-alpha");
-                    return Behaviors.receiveMessage(
-                            msg -> {
-                                if (msg instanceof Start start) {
-                                    context.getLog().info("starting");
-                                    manager.tell(new Manager.Delegate(start.texts));
-                                    return Behaviors.same();
-                                } else
-                                    return Behaviors.unhandled();
-                            });
-//                    return BehaviorBuilder.<Command>create()
-//                            .onMessage(
-//                                    Guardian.Start.class,
-//                                    start -> {
-//                                        manager.tell(new Manager.Delegate(start.texts));
-//                                        return Behaviors.same();
-//                                    })
-//                            .build();
-                });
+    public record Start(ArrayList<String> texts) implements Command {
     }
 }
 
 class Manager {
 
-    interface Command {
+    public static Behavior<Command> create() {
+        return Behaviors.setup(context -> {
+            ActorRef<Worker.Response> adapter = context.messageAdapter(Worker.Response.class, WorkerDoneAdapter::new);
+            return BehaviorBuilder.<Command>create().onMessage(Delegate.class, delegate -> {
+                delegate.texts.forEach(text -> {
+                    ActorRef<Worker.Command> worker = context.spawn(Worker.create(), text);
+                    context.getLog().info("sending text '{}' to worker", text);
+                    worker.tell(new Worker.Parse(adapter, text));
+                });
+                return Behaviors.same();
+            }).onMessage(WorkerDoneAdapter.class, workerDone -> {
+                context.getLog().info("text '{}' has been finished", ((Worker.Done) (workerDone.response)).text());
+                return Behaviors.same();
+            }).build();
+        });
     }
 
-//    static final class Delegate implements Command {
-//        String[] texts;
-//
-//        Delegate(String[] texts) {
-//            this.texts = texts;
-//        }
-//    }
+    public sealed interface Command {
+    }
 
-    record Delegate(String[] texts) implements Command {
+    public record Delegate(ArrayList<String> texts) implements Command {
     }
 
     private record WorkerDoneAdapter(Worker.Response response) implements Command {
-    }
-
-    static Behavior<Command> create() {
-        return Behaviors.setup(
-                context -> {
-                    context.getLog().info("setting up. Creating worker");
-                    ActorRef<Worker.Response> adapter =
-                            context.messageAdapter(Worker.Response.class, WorkerDoneAdapter::new);
-                    return BehaviorBuilder.<Command>create()
-                            .onMessage(
-                                    Delegate.class,
-                                    delegate -> {
-                                        Stream.of(delegate.texts)
-                                                .forEach(
-                                                        text -> {
-                                                            ActorRef<Worker.Command> worker =
-                                                                    context.spawn(Worker.create(), text);
-                                                            context.getLog().info("sending text '{}' to worker", text);
-                                                            worker.tell(new Worker.Parse(adapter, text));
-                                                        });
-                                        return Behaviors.same();
-                                    })
-                            .onMessage(
-                                    WorkerDoneAdapter.class,
-                                    workerDone -> {
-                                        context.getLog().info("text '{}' has been finished.", workerDone.response);
-                                        return Behaviors.same();
-                                    })
-                            .build();
-                });
     }
 }
 
 class Worker {
 
-    interface Command {
+    public static Behavior<Command> create() {
+        return Behaviors.setup(context -> BehaviorBuilder.<Command>create().onMessage(Parse.class, parse -> {
+            String parsed = naiveParsing(parse.text);
+            context.getLog().info("'{}' DONE!. Parsed result: {}", context.getSelf(), parsed);
+            parse.replyTo.tell(new Done(parse.text));
+            return Behaviors.stopped();
+        }).build());
     }
 
-//    static final class Parse implements Command {
-//        ActorRef<Response> replyTo;
-//        String text;
-//
-//        Parse(ActorRef<Response> replyTo, String text) {
-//            this.replyTo = replyTo;
-//            this.text = text;
-//        }
-//    }
-
-    record Parse(ActorRef<Response> replyTo, String text) implements Command {
+    public static String naiveParsing(String text) {
+        return text.replace("-", "");
     }
 
-    interface Response {
+    public interface Command {
     }
 
-//    static final class Done implements Response {
-//        String text;
-//
-//        Done(String text) {
-//            this.text = text;
-//        }
-//    }
 
-    record Done(String text) implements Response {
+    public sealed interface Response {
     }
 
-    static Behavior<Command> create() {
-        return Behaviors.setup(
-                context ->
-                        BehaviorBuilder.<Command>create()
-                                .onMessage(
-                                        Parse.class,
-                                        parse -> {
-                                            String parsed = naiveParsing(parse.text);
-                                            context
-                                                    .getLog()
-                                                    .info("'{}' DONE!. Parsed result: {}", context.getSelf(), parsed);
-                                            parse.replyTo.tell(new Done(parsed));
-                                            return Behaviors.stopped();
-                                        })
-                                .build());
+    public record Parse(ActorRef<Response> replyTo, String text) implements Command {
     }
 
-    static String naiveParsing(String text) {
-        return text.replaceAll("-", "");
+    public record Done(String text) implements Response {
     }
 }
